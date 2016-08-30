028b7db5fc4b77c170f67357f3ebf93f3de4e5dd
richelbilderbeek <richel@richelbilderbeek.nl>
2016-08-30 15:09:12 +0200

    Corrected build scripts

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-qmake travis_qmake_gcc_cpp98_boost_test_gcov.pro
+qmake travis_qmake_gcc_cpp98_boost_test_gcov_oclint.pro
 make
-./travis_qmake_gcc_cpp98_boost_test_gcov
--- build_test.sh
+++ build_test.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-qmake travis_qmake_gcc_cpp98_boost_test_gcov_test.pro
+qmake travis_qmake_gcc_cpp98_boost_test_gcov_oclint_test.pro
 make
-./travis_qmake_gcc_cpp98_boost_test_gcov_test
